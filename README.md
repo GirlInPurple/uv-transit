a04c4715ba4bae85c0c362f6edac0c0137dfe1a3
The Color Blurple <turtlenerd603@gmail.com>
2024-07-08 03:00:05 -0400

    Complete Rewrite

--- README.md
+++ README.md
@@ -1,32 +1,71 @@
 # UVDOT UltraVanilla Transit Router
 
-This page was moved from the original UV website due to the amount of upkeep it needs, along with having less oversight and not having to bother the admins to accept a PR every week or so.
+This page was moved from the original UV website due to the amount of upkeep it needs, along with not having to bother the admins to accept a PR every week or so to update the graph.\
+It was also partly rewritten to make it slightly easier to maintain, this time around taking full advantage of jQuery and proper nesting/scoping.\
 
 Licensed under AGPL v3 as to keep compatibility with [UltraVanilla/uvweb](https://github.com/UltraVanilla/uvweb), [416.png](./416.png) is CC-BY-SA &copy;UltraVanilla 2024.
 
-## Routing Methods
+## Technical Info
 
-### Routing Basics / Fastest Route
-
-There is a single function that controls all the routing processes, from data manipulation to tracking how long it takes.\
+There is a single function that controls all the routing processes, from data manipulation to tracking how long it takes.
 This is activated by the user clicking the "Route!" button.
 
-The function then takes all the settings from the user, along with the location graph, and creates a node graph that is parsable by A-Star.\
-This is the longest part of the base process due to the sheer amount of data it needs to handle.
+The function then takes all the settings from the user, along with the location graph, and creates a node graph that is parsable by A-Star. This is the longest part of the base process due to the sheer amount of data it needs to handle.\
+During this stage, weights can be altered in either direction, up or down, depending on the factors the user chooses. This is primarily used to avoid tolls and for the alternative routing methods.
 
 The AStar function is the part where the actual routing happens, based on the [A* pathing algorithm](https://en.wikipedia.org/wiki/A*_search_algorithm). It sends back a list of nodes and methods it followed to get this route, or an error message if one occurs.
 
-From here, it takes the output from the router, parses it, and and displays it on the page, with extra formatting, road and transit line names, and more.
-
-### Least Transfers
-
-For this routing method, during the generation of the node graph, the routing weights for transfers out of stations or onto different roads are quadrupled, making the routing algorithm choose them much less often.
-
-### Scenic / Longest Route
-
-This one is the most complex and costly, but ends up working pretty well.\
-This method runs the "Fastest Route" method 10 times, but each time the weights going into all the nodes the router hit are quadrupled, making the router very unhappy and taking the most round-about route possible. This route only works if the route is restricted to Overworld and/or slower methods, as if everything was available the router will keep taking lesser but still efficient routes of getting to your destination.
+From here, all the data from the pathing and compiling functions get passed into the rendering functions. This is a text based explanation similar to
 
 ## Adding Location Nodes
 
-TODO
+Here is an example of a location node:
+
+```javascript
+const routes = {
+    // ...
+    "Outpost": [
+        [-1890764,-1894658],
+        {
+            roadways: {
+                "UV-OP-201 UV-OP-2 Intersection": [["UV-OP-2"], 1200],
+
+                "Evergreen": [["UV-OP-1", "UV-VY"], 1200],
+                "Stonehelm": [["UV-OP-1", "UV-OP-2", "UV-VY"], 1200],
+            },
+            railways: {
+                "Outpost UltraStar Station": [["Toll"], 0, {
+                    currency: "Emerald",
+                    price: 4,
+                    pass: "SeaCard",
+                    passPrice: 2
+                }]
+            },
+            iceways: {
+                "Illyria": [["OPE Ouest"], 1200],
+                "Stonehelm": [["EGRK Expressway"], 1200],
+            },
+        },
+    ]
+    // ...
+}
+```
+
+Lets go from top to bottom:
+
+- First is the node name. If its a town or destination it should be the same as the one in the `places` variable, and the name has to be unique.
+- Next is the coordinates, the Y coordinate is dropped due to it not being useful to us.
+- Lastly, the routes
+  - Each route is formatted in this manner:
+    - Node Name
+    - Transit Name(s)
+    - Distance in Blocks/Meters
+    - Toll (if applicable)
+      - The toll is again split up into 4 sections:
+        - `currency` is the type of currency the toll requires. Common currencies are Diamond, Emerald, and Paper.
+        - `price` is the amount of that currency needed to pass the toll.
+        - `pass` is the name of the pass that allows you to skip the toll. Currently only SeaCard and EasyPass are options.
+        - `passPrice` is the price that is paid if the user checked the box saying they have a specific pass.
+  - The `roadways` section is for connected roadways, be it UVDOT highways or other nearby roads that aren't numbered.
+    - The space between "UV-OP-201 UV-OP-2 Intersection" and "Evergreen" is to tell people who are adding onto the network graph that the "UV-OP-201 UV-OP-2 Intersection" node can not be selected from the UI, but still exists. Same thing with the "Outpost UltraStar Station" node, but this time there are no other connections so the space doesnt make sense to add.
+  - The `railways` and `iceways` sections are for minecart railways and ice boat tunnels respectively.
---
+++ data.js
@@ -0,0 +1,248 @@
+//! Please read the entire README.md file before attempting to edit this.
+//! I would prefer you make an issue instead if you want something added.
+
+/*
+(most) of the major places/towns in the world.
+Add towns in Alphabetical order, underscores "_" are disallowed, and keep capitalization in mind!
+This list is copy-pasted from the wiki: https://wiki.ultravanilla.world/wiki/Category:Settlement
+*/
+let places = {
+    "Amanita": "outpost",
+    "Aquileia": "outpost",
+    "Aurora": "old-spawn",
+    "Banal-Witchita": "outpost",
+    "Birch Island": "old-spawn",
+    "Borealis": "new-spawn",
+    "Burzum": "old-spawn",
+    "Cara Island": "old-spawn",
+    "Cara Nova": "new-spawn",
+    "Cloud City": "new-spawn",
+    "Colex": "old-spawn",
+    "Condor's Nest": "old-spawn",
+    "Cyprus": "old-spawn",
+    "Darkshire": "new-spawn",
+    "Dìxià Chéng": "new-spawn",
+    "Eden": "outpost",
+    "Ekilorea": "outpost",
+    "Enchanted Isles": "old-spawn",
+    "Evergreen": "outpost",
+    "Freedom City": "new-spawn",
+    "Frogtopia": "outpost",
+    "Gold Spur": "old-spawn",
+    "Hana Reservoir": "old-spawn",
+    "HoboNation": "old-spawn",
+    "Illyria": "outpost",
+    "Krill Lanka": "outpost",
+    "Malbork": "new-spawn",
+    "Malus": "outpost",
+    "Mercy": "old-spawn",
+    "Merk Town": "new-spawn",
+    "Naval Detroit": "old-spawn",
+    "New Spawn": "new-spawn",
+    "Old Mann": "old-spawn",
+    "Old Spawn": "old-spawn",
+    "Outpost": "outpost",
+    "Pagasa City": "outpost",
+    "Primitown": "old-spawn",
+    "Rotherhythe": "outpost",
+    "STEAMEDHAMS": "new-spawn",
+    "Scintilla": "old-spawn",
+    "Seascrestia": "outpost",
+    "Siberia": "old-spawn",
+    "Sin City": "outpost",
+    "Sky Temple": "new-spawn",
+    "Solitude": "new-spawn",
+    "Southern Hollow": "old-spawn",
+    "Spanish Settlements": "new-spawn",
+    "Steambolt": "outpost",
+    "Stonehelm": "outpost",
+    "Tegridy": "outpost",
+    "Terni": "old-spawn",
+    "Terra": "outpost",
+    "The Swamp": "old-spawn",
+    "Totemia": "new-spawn",
+    "Valhalla": "old-spawn",
+    "Vault 11": "new-spawn",
+    "Vault 17": "old-spawn",
+    "Waterthorn": "old-spawn",
+    "WestHaven": "old-spawn",
+    "Willichburg": "old-spawn",
+    "Wolven": "old-spawn",
+}
+
+/*
+All the named methods of transport.
+Mostly consists of roads, iceways, and train lines.
+*/
+const methods = {
+    roadways: {
+
+    },
+    railways: {
+
+    },
+    iceways: {
+
+    },
+    pistonbolts: {
+
+    },
+    boatways: {
+
+    }
+}
+
+/*
+All possible routes between locations, in a semi-human readable format.
+*/
+const routes = {
+    "Banal-Witchita": [
+        [-1890005,-1898735],
+        {
+            roadways: {
+                "Evergreen": [["UV-OP-1", "UV-VY"], 3000],
+                "UV-OP-1 Ekilorea Intersection": [["UV-OP-1", "UV-VY"], 2000],
+            },
+        },
+    ],
+    "Ekilorea": [
+        [-1888319,-1899481],
+        {
+            roadways: {
+                "UV-OP-1 Ekilorea Intersection": [["Road to Ekilorea Island"], 1200],
+            },
+        },
+    ],
+    "Evergreen": [
+        [-1890899,-1896049],
+        {
+            roadways: {
+                "UV-OP-201 UV-OP-2 Intersection": [["UV-OP-201"], 1500],
+
+                "Banal-Witchita": [["UV-OP-1", "UV-VY"], 3000],
+            },
+            railways: {
+                "Outpost": [["UltraStar 1"], 1600, {
+                    currency: "Emerald",
+                    price: 4,
+                    pass: "SeaCard",
+                    passPrice: 2
+                }]
+            },
+            iceways: {
+                //"Stonehelm": [["EGRK Expressway"], 1200]
+            },
+        },
+    ],
+    "Illyria": [
+        [-1894692,-1894826],
+        {
+            roadways: {
+                "UV-OP-203 UV-OP-2 Intersection": [["UV-OP-2"], 1200],
+            },
+            railways: {
+                "Pagasa City": [["UltraStar 2"], 1200, {
+                    currency: "Emerald",
+                    price: 4,
+                    pass: "SeaCard",
+                    passPrice: 2
+                }]
+            },
+            iceways: {
+                "Outpost": [["OPE Ouest"], 1200]
+            },
+        },
+    ],
+    "Outpost": [
+        [-1890764,-1894658],
+        {
+            roadways: {
+                "UV-OP-201 UV-OP-2 Intersection": [["UV-OP-2"], 1200],
+
+                "Evergreen": [["UV-OP-1", "UV-VY"], 1200],
+                //"Stonehelm": [["UV-OP-1", "UV-OP-2", "UV-VY"], 1200],
+            },
+            railways: {
+                "Seascrestia": [["UltraStar 1"], 1200, {
+                    currency: "Emerald",
+                    price: 4,
+                    pass: "SeaCard",
+                    passPrice: 2
+                }],
+                "Evergreen": [["UltraStar 2"], 1200, {
+                    currency: "Emerald",
+                    price: 4,
+                    pass: "SeaCard",
+                    passPrice: 2
+                }],
+            },
+            iceways: {
+                "Illyria": [["OPE Ouest"], 1200],
+                //"Stonehelm": [["EGRK Expressway"], 1200],
+            },
+        },
+    ],
+    "Pagasa City": [
+        [-1893328,-1894502],
+        {
+            roadways: {
+                "UV-OP-203 UV-OP-2 Intersection": [["UV-OP-203"], 1200],
+            },
+            railways: {
+                "Seascrestia": [["UltraStar 2"], 1200, {
+                    currency: "Emerald",
+                    price: 4,
+                    pass: "SeaCard",
+                    passPrice: 2
+                }],
+                "Illyria": [["UltraStar 1"], 1200, {
+                    currency: "Emerald",
+                    price: 4,
+                    pass: "SeaCard",
+                    passPrice: 2
+                }],
+            },
+        },
+    ],
+    "Seascrestia": [
+        [-1891664,-1893972],
+        {
+            railways: {
+                "Outpost": [["UltraStar 2"], 1200, {
+                    currency: "Emerald",
+                    price: 4,
+                    pass: "SeaCard",
+                    passPrice: 2
+                }],
+                "Pagasa City": [["UltraStar 1"], 1200, {
+                    currency: "Emerald",
+                    price: 4,
+                    pass: "SeaCard",
+                    passPrice: 2
+                }]
+            },
+        },
+    ],
+    "UV-OP-201 UV-OP-2 Intersection": [
+        [-1891962,-1895207],
+        {
+            roadways: {
+                "UV-OP-203 UV-OP-2 Intersection": [["UV-OP-2"], 1200],
+
+                "Evergreen": [["UV-OP-201"], 1200],
+                "Outpost": [["UV-OP-2"], 1200],
+            },
+        },
+    ],
+    "UV-OP-203 UV-OP-2 Intersection": [
+        [-1893230,-1895039],
+        {
+            roadways: {
+                "UV-OP-201 UV-OP-2 Intersection": [["UV-OP-2"], 1200],
+
+                "Illyria": [["UV-OP-2"], 1200],
+                "Pagasa City": [["UV-OP-203"], 1200],
+            },
+        },
+    ],
+}
